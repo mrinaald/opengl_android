db5046fdc2f6e6005a722c693dfd41da72dd4aec
Mrinaal Dogra <mrinaald@iitk.ac.in>
2020-01-19 02:29:04 +0530

    Create a GL Surface

--- OpenGLAndroid/app/src/main/cpp/native-lib.cpp
+++ OpenGLAndroid/app/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_openglandroid_MainActivity_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
+        jobject obj /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
--- OpenGLAndroid/app/src/main/java/com/example/openglandroid/MainActivity.kt
+++ OpenGLAndroid/app/src/main/java/com/example/openglandroid/MainActivity.kt
@@ -2,23 +2,43 @@ package com.example.openglandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.opengl.GLSurfaceView
+import android.util.Log
+
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        gl_surface_view.setEGLContextClientVersion(2)
+        gl_surface_view.setRenderer(Renderer())
+        gl_surface_view.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+    private class Renderer : GLSurfaceView.Renderer {
+        override fun onDrawFrame(gl: GL10?) {
+
+        }
+
+        override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+
+        }
+
+        override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+            gl?.glGetString(GL10.GL_VERSION).also {
+                Log.d(this::class.java.simpleName, "GL Version: $it")
+            }
+        }
+
+    }
 
     companion object {
 
