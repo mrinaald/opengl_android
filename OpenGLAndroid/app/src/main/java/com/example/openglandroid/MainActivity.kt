b6fcd3344dfe9c79f5c728814d08b80b65524818
Mrinaal Dogra <mrinaald@iitk.ac.in>
2020-01-19 03:21:44 +0530

    Include OpenGL ES Version check and fallback

--- OpenGLAndroid/app/src/main/java/com/example/openglandroid/MainActivity.kt
+++ OpenGLAndroid/app/src/main/java/com/example/openglandroid/MainActivity.kt
@@ -9,18 +9,51 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+import android.content.pm.ConfigurationInfo
+import android.app.ActivityManager
+import android.content.Context
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = MainActivity::class.java.simpleName
+    var mGLESVersion : Int = 0
+    var mSupportGL3x : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gl_surface_view.setEGLContextClientVersion(2)
+        checkGLVersionCompatibility()
+        gl_surface_view.setEGLContextClientVersion(if (mSupportGL3x) 3 else 2)
+
         gl_surface_view.setRenderer(Renderer())
         gl_surface_view.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+    }
+
+    private fun checkGLVersionCompatibility ()  {
+        mGLESVersion = getVersionFromActivityManager(this)
+
+        val majorVersion = ((mGLESVersion and 0xFFFF0000.toInt())) shr 16
+        val minorVersion = (mGLESVersion and 0xFFFF)
+        Log.d(TAG, "OpenGL Version from Activity Manager: ${majorVersion}.${minorVersion}")
+
+        mSupportGL3x = majorVersion == 3
+    }
+
+    /**
+     * This function is taken directly from
+     * https://android.googlesource.com/platform/cts/+/2b87267/tests/tests/graphics/src/android/opengl/cts/OpenGlEsVersionTest.java
+     */
+    private fun getVersionFromActivityManager(context: Context): Int {
+        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val configInfo = activityManager.deviceConfigurationInfo
+        return if (configInfo.reqGlEsVersion != ConfigurationInfo.GL_ES_VERSION_UNDEFINED) {
+            configInfo.reqGlEsVersion
+        } else {
+            1 shl 16 // Lack of property means OpenGL ES version 1
+        }
     }
 
     private class Renderer : GLSurfaceView.Renderer {
